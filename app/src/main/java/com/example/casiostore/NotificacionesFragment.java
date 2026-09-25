88122afee8fdc82ad110b7055fc6edaeaa175fde
veru <veruscaorellanarodas@gmail.com>
2026-09-24 23:56:45 -0400

    NoticacionesFragment,fragment_notificaciones,item_nptificacion

---
+++ app/src/main/java/com/example/casiostore/NotificacionesFragment.java
@@ -0,0 +1,23 @@
+package com.example.casiostore;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NotificacionesFragment extends Fragment {
+
+    public NotificacionesFragment() {
+        // Constructor vacío requerido
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notificaciones, container, false);
+    }
+}
